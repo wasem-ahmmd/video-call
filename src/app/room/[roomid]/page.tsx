6a129b1d7bdeb03ec3517f96d432c9a60c6d4649
wasem-ahmmd <wasemahmmd@gmail.com>
2024-06-17 07:05:33 -0700

    new app video

---
+++ src/app/room/[roomid]/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import * as React from "react";
+import { useParams } from "next/navigation";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const page = () => {
+  const { roomid }: {roomid : string} = useParams();
+
+  const myMeeting = async (element: HTMLDivElement) => {
+    const appId = 1938814673;
+    const serverSecret = "f34facb4aa7b8937969368781e0a1838";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appId,
+      serverSecret,
+      roomid,
+      Date.now().toString(),
+      "waseem ahmed"
+    );
+    const zc = ZegoUIKitPrebuilt.create(kitToken)
+    zc.joinRoom({
+      container: element,
+      sharedLinks:[
+        {
+          name: "copy link",
+          url: `http://localhost:3000/room/${roomid}`
+        }
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      }
+    })
+  };
+  const setRef = (element: HTMLDivElement | null) => {
+      if (element) {
+        myMeeting(element);
+      }
+    }
+  return <div>
+    <div ref={setRef} />
+  </div>;
+};
+
+export default page;
